061cccb4e65a16cad64016ffdbd660dd68b7d768
sshi <shuxin.shi@mines-paristech.fr>
2023-10-14 15:13:37 +0200

    feat(checkpoints): added checkpoint.py for managing model saving and loading

---
+++ src/utils/checkpoint.py
@@ -0,0 +1,14 @@
+import torch
+
+def save_checkpoint(epoch, model, optimizer, checkpoint_path):
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+    }, checkpoint_path)
+
+def load_checkpoint(model, optimizer, checkpoint_path):
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    return checkpoint['epoch']
