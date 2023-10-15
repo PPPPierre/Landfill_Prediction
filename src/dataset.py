34c6bbbd33d68a3e38b83a37b8fd80665c6f0eae
sshi <shuxin.shi@mines-paristech.fr>
2023-10-15 09:21:46 +0200

    perf(dataset): improved dataset definition, added more params to control the data loading

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,9 @@
+# Basic lib
 import os
-from typing import Union, Optional
+import sys
+from typing import Union, Optional, List
 
+# Lib for data collection
 import geopandas as gpd
 from pystac.extensions.eo import EOExtension as eo
 from pystac_client import Client
@@ -8,12 +11,17 @@ import planetary_computer
 import rasterio
 from rasterio import windows, features, warp
 
+# Lib for ML
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 
-from .augmentation import get_transforms_from_config
+# Porject lib
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from src.augmentation import get_transforms_from_config
 
 root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,31 +30,34 @@ def get_dataloader_from_cfg(cfg: dict):
     # Load arguments
     geojson_path = cfg["geojson_path"]
     transform = get_transforms_from_config(cfg["transforms"])
+    collections = cfg["collections"]
     datetime = cfg["datetime"]
     batch_size = cfg.get("batch_size", 4)
     shuffle = cfg.get("shuffle", True)
     download_dir = cfg.get("download_dir", None)
 
     # Loading dataset and dataloader
-    dataset = SatelliteDataset(geojson_path, datetime=datetime, download_dir=download_dir, transform=transform)
+    dataset = SatelliteDataset(geojson_path, collections=collections, datetime=datetime, download_dir=download_dir, transform=transform)
     loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
 
     return dataset, loader
 
 
 class SatelliteDataset(Dataset):
-    def __init__(self, geojson_path: str, datetime: str, download_dir: Optional[str]=None, transform=None) -> None:
+    def __init__(self, geojson_path: str, collections: str="sentinel-2-l2a", datetime: str="2022-01-01/2022-12-30", band: str="visual", download_dir: Optional[str]=None, transform=None) -> None:
         self.data = gpd.read_file(geojson_path)
-        self.filename = os.path.basename(geojson_path).replace(".geojson", "")
+        self.collections = collections
         self.datetime = datetime
+        self.band = band
         self.transform = transform
         self.catalog = Client.open(
             "https://planetarycomputer.microsoft.com/api/stac/v1",
             modifier=planetary_computer.sign_inplace,
         )
 
+        filename = f"{os.path.basename(geojson_path).replace('.geojson', '')}_{collections}_{datetime.replace('/', '_')}_{band}"
         if download_dir is None:
-            self.download_dir = os.path.join(root, f"data/temp/{self.filename}")
+            self.download_dir = os.path.join(root, f"data/temp/{filename}")
         else:
             self.download_dir = download_dir
         
@@ -63,12 +74,11 @@ class SatelliteDataset(Dataset):
         area_of_interest = self.data.iloc[idx]['geometry']
         label = self.data.iloc[idx]['label']
         data_id = self.data.iloc[idx]['id']
-
         data_path = os.path.join(self.download_dir, f"data_{data_id}.pt")
         
         if not os.path.exists(data_path):
             search = self.catalog.search(
-                collections=["sentinel-2-l2a"],
+                collections=[self.collections],
                 intersects=area_of_interest,
                 datetime=self.datetime,
                 query={"eo:cloud_cover": {"lt": 10}}
@@ -76,7 +86,7 @@ class SatelliteDataset(Dataset):
 
             items = search.item_collection()
             least_cloudy_item = min(items, key=lambda item: eo.ext(item).cloud_cover)
-            asset_href = least_cloudy_item.assets["visual"].href
+            asset_href = least_cloudy_item.assets[self.band].href
 
             with rasterio.open(asset_href) as ds:
                 aoi_bounds = features.bounds(area_of_interest)
@@ -124,12 +134,14 @@ if __name__ == '__main__':
         },
     ]
 
-    composed_transforms = get_transforms_from_config(transforms_list)
-    geojson_path = "./data/raw/train.geojson"
+    collections = "sentinel-2-l2a"
+    geojson_path = "./data/raw/test.geojson"
     datetime = "2022-01-01/2022-12-30"
-    dataset = SatelliteDataset(geojson_path, datetime, transform=composed_transforms)
+    band = "visual"
+    composed_transforms = get_transforms_from_config(transforms_list)
+    dataset = SatelliteDataset(geojson_path, collections, datetime, band, transform=None)
 
     for img, label in dataset:
-        print(f"Origin size: {img.size()}, label: {label}")
-        plt.imshow(img.permute(1, 2, 0))
-    plt.show()
+        print(f"Origin size: {img.size}, label: {label}")
+    #     plt.imshow(img.permute(1, 2, 0))
+    # plt.show()
